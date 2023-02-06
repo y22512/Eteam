d2c7a32d9d5a9cb045f1937110b2738ac4c8ddb1
y22519 <123909622+y22519@users.noreply.github.com>
2023-02-06 12:06:56 +0900

    2/6　途中
    a

--- main.js
+++ main.js
@@ -9,6 +9,141 @@
     5,勝ったら掛け金の1.5倍を返す
       ブラックジャック(21)なら２倍にして返す
 */
-var my_points;      //自分のポイント
-var dealer_points;  //ディーラーのポイント
-//const card_img[];
+var my_points=[0,0];      //自分のポイント[点数,1の個数]
+var dealer_points=[0,0];  //ディーラーのポイント[点数,1の個数]
+var bed_Score=0;//ベッド数
+var turns_number=0;//ターン数
+var my_card;//自分の持っているカード
+var dealer_card;//相手が持っているカード
+var card_correct = new Array(4);//画像の確認配列
+//2次元目の画像配列
+for(let i=0;i<4;i++)
+{
+  card_img[i]=new Array(13).fill(1);
+};
+//ランダム生成関数
+function create_random_13()
+{
+  return Math.floor(Math.random()*100%13);
+};
+function create_random_4()
+{
+  return Math.floor(Math.random()*100%4);
+};
+
+//配られたカードを持っているカードに追加
+function add_distribute_card(type_number,sheets_number,person)
+{
+  //プレイヤー側の追加
+  if(person==1)
+  {               //ターン数　　ハートとか　　数字
+    my_card.push([turns_number,type_number,sheets_number]);
+    //ジャック、クイーン、キング、ではないかの確認
+    if(sheets_number<=10)
+    {
+      my_points[0]+=sheets_number+1;
+    }
+    //ジャック、クイーン、キングは一律10;
+    else
+    {
+      my_points[0]+=sheets_number+10;
+    }
+    //1があるか
+    if(sheets_number==0)
+    {
+      my_points[1]+=1;
+    }
+  }
+
+  
+  //ディーラー側の追加
+  if(person==0)
+  {
+    dealer_card.push([turns_number,type_number,sheets_number]);
+  }
+  //ジャック、クイーン、キング、ではないかの確認
+  if(sheets_number<=10 && sheets_number!=0)
+  {
+    dealer_points[0]+=sheets_number+1;
+  }
+  //ジャック、クイーン、キングは一律10;
+  else
+  {
+    dealer_points[0]+=sheets_number+10;
+  }
+  //1があるか
+  if(sheets_number==0)
+  {
+    dealer_points[1]+=1;
+  }
+};
+
+//カードを配る
+function distribute_card(person)
+{
+  let draw_type_number = create_random_4();
+  let draw_sheets_number=create_random_13();
+  while(card_correct[draw_type_number][draw_sheets_number]==0)
+  { 
+    draw_type_number = create_random_4();
+    draw_sheets_number=create_random_13();
+  };
+  //ドローしたので0にする
+  card_correct[draw_type_number][draw_sheets_number]=0
+  //ドローしたカードを追加する
+  add_distribute_card(draw_type_number,draw_sheets_number,person)
+
+}
+
+//点数計算
+function put_out_sum(points)
+{
+  while(points[0]<=21-11 && points[1]>0)
+  {
+    points[0]+=11;
+    points[1]-=1;
+  }
+  return points[0]+points[1];
+}
+
+//ディーラーのカードを引くかひかないかを決める
+function correct_AI()
+{
+  if(put_out_sum(dealer_points)<17)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+//1.ベッド数を入力
+document.querySelector().addEventListener("click",function()
+{
+  bed_Score = document.querySelector();
+  //カードを2枚ドロー
+  for(let i=0;i<2;i++)
+  {
+    //プレイヤー側
+    distribute_card(1)
+
+    //ディーラー側
+    distribute_card(0)
+
+    turns_number+=1;
+  };
+});
+
+//ヒットボタン
+document.querySelector().addEventListener("click",function()
+{
+  //プレイヤー側
+  distribute_card(1);
+  //ディーラー側
+  if(correct_AI==1)
+  {
+    distribute_card(0);
+  }
+});
